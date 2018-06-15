7e0c58ff5f92579230d4b47bd7bcb2f43df36ea7
ALCIDE Frederic - CGI <frederic.alcide@rd.loreal.com>
2018-06-15 15:47:11 +0200

    Restarting front JS using global for now

--- public/js/home.js
+++ public/js/home.js
@@ -1,193 +1,166 @@
-let audioCtx, masterGain, songPlaying;
-const song = document.getElementById('audioSource');
-const sliders = document.querySelectorAll('.slider');
+const audio = (() => {
 
-// ==============================================
-// Setup Context
-// ==============================================
+    const audio = {};
+    const ctx = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillators = {};
+    const gains = {};
+    const analysers = {};
 
-function audioSetup() {
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    const songSource = audioCtx.createMediaElementSource(song);
+    function initialize() {
+        // Déclaration du gain global
+        const masterGain = ctx.createGain();
+        masterGain.connect(ctx.destination);
+        gains.master = masterGain;
 
-    masterGain = audioCtx.createGain();
-    masterGain.gain.value = 0.3;
+        // Déclaration de l'analyser global
+        analysers.master = ctx.createAnalyser();
+        gains.master.connect(analysers.master);
+    }    
 
-    songSource.connect(masterGain);
-    masterGain.connect(audioCtx.destination);
-}
+    initialize();
 
-audioSetup();
+    function isPlaying(analyser) {
+        const data = new Uint8Array(analyser.fftSize);
+        analyser.getByteTimeDomainData(data);
 
-
-// ==============================================
-// Sliders Handlers
-// ==============================================
-
-sliders.forEach(slider => {
-    slider.addEventListener('click', window[slider.dataset.method]);
-    slider.addEventListener('mousedown', handleDrag);
-});
-
-// Get the coefficient between the width and offset of the slider
-function getCoefficient(event) {
-    const slider = event.currentTarget;
-    const rect = slider.getBoundingClientRect();
-    const offset = event.clientX - slider.offsetLeft;
-    const width = rect.width;
-
-    return offset / width;
-}
-
-// Handle hook and unhook of the drag event
-function handleDrag(event) {
-    const slider = event.currentTarget;
-    const previousState = songPlaying;
-    const isPlayback = slider.classList.contains('playback-slider');
-
-    if (previousState && isPlayback) {
-        pauseSong(); // If we don't pause here the song continue to play while moving (bad)
+        const isPlaying = data.some((v) => v !== 128);
+        
+        return isPlaying;
     }
-    this.addEventListener('mousemove', window[slider.dataset.method], false);
-    this.addEventListener('mouseup', function tempCb() {
-        this.removeEventListener('mousemove', window[slider.dataset.method], false);
-        this.removeEventListener('mouseup', tempCb, false);
-        if (previousState && isPlayback) {
-            playSong();
-        }
-    }, false);
-}
 
-// Moving the current time of the song to the mouse location
-function slideTime(event) {
-    const x = event.clientX - event.currentTarget.offsetLeft;
-    song.currentTime = song.duration * getCoefficient(event);
-}
-
-// Moving the volume to the mouse position
-function slideVolume(event) {
-    const x = event.clientX - event.currentTarget.offsetLeft;
-    song.volume = getCoefficient(event);
-    drawVolume();
-}
-
-
-// ==============================================
-// Play / Pause Handlers
-// ==============================================
-const startBtn = document.getElementById('startBtn');
-
-startBtn.addEventListener('click', () => {
-    if (songPlaying) {
-        pauseSong();
-    } else {
-        playSong();
+    function createAnalyser(name, node) {
+        const analyser = analysers[name] || ctx.createAnalyser();
+        node.connect(analyser);
+        analysers[name] = analyser;
     }
-});
 
-function playSong() {
-    if(!songPlaying) {
-        song.play();
-        drawOscilloscope();
-        updateFreqWaveForm();
-        drawPlayback();
-        startBtn.innerText = 'Pause';
-        songPlaying = true;
+    function deleteAnalyser(name, node) {
+        node.disconnect(analysers[name]);
+        analysers[name] = null;
     }
-}
 
-function pauseSong() {
-    if(songPlaying) {
-        song.pause();
-        drawPlayback();
-        startBtn.innerText = 'Start';
-        songPlaying = false;
-    }
-}
+    audio.createOscillator = function(name, type, freq) {
 
-// =================================================
-// Draw volume
-// =================================================
-const volumeProgress = document.getElementById('volume-progress');
-
-function drawVolume() {
-    const progress = song.volume * 100;
-    volumeProgress.style.width = `${progress}%`;
-}
-
-
-// =================================================
-// Draw Playback
-// =================================================
-const playbackProgress = document.getElementById("playback-progress");
-
-function drawPlayback() {
-    requestAnimationFrame(drawPlayback);
-    const progress = (song.currentTime / song.duration) * 100;
-    playbackProgress.style.width = `${progress}%`;
-}
-
-
-// ==============================================
-// Create Frequency Wave Form
-// ==============================================
-const analyser = audioCtx.createAnalyser();
-const freqWaveform = new Float32Array(analyser.frequencyBinCount);
-const freqWaveform2 = new Uint8Array(analyser.frequencyBinCount);
-
-masterGain.connect(analyser);
-analyser.getFloatFrequencyData(freqWaveform);
-analyser.getByteFrequencyData(freqWaveform2);
-
-function updateFreqWaveForm() {
-    requestAnimationFrame(updateFreqWaveForm);
-    analyser.getFloatFrequencyData(freqWaveform);
-    analyser.getByteFrequencyData(freqWaveform2);
-}
-
-
-// ==============================================
-// Draw Oscilloscope
-// ==============================================
-
-function drawOscilloscope() {
-    requestAnimationFrame(drawOscilloscope);
-
-    const scopeCanvas = document.getElementById('oscillator');
-    const scopeContext = scopeCanvas.getContext('2d');
-
-    scopeCanvas.width = window.innerWidth;
-    scopeCanvas.height = scopeCanvas.getBoundingClientRect().height;
-
-    scopeContext.clearRect(0, 0, scopeCanvas.width, scopeCanvas.height);
-    scopeContext.beginPath();
-
-    const waveforms = [freqWaveform, freqWaveform2];
-
-    waveforms.forEach((waveform, index) => {
-        const delta = window.innerWidth / waveform.length;
-
-        for (let i = 0; i < waveform.length; i++) {
-            const x = i * delta;
-            let y = 0;
-
-            if(index === 0) {
-                const scale = 255 / (analyser.maxDecibels - analyser.minDecibels);
-                const offset = analyser.minDecibels;
-                y = - ((waveform[i] - offset) * scale);
-            } else {
-                y = scopeCanvas.height - 100 - waveform[i];
-            }
-
-            if (i == 0) {
-                scopeContext.moveTo(x, y);
-            } else {
-                scopeContext.lineTo(x, y);
-            }
+        if(!name || !type || !freq) {
+            throw `The waveform and the frequence are mandatory.`;
         }
 
-        scopeContext.strokeStyle = index === 0 ? 'red' : 'green';
-        scopeContext.lineWidth = 1;
-        scopeContext.stroke();
-    });
-}
+        name = name.toLowerCase();
+        type = type.toLowerCase();
+
+        if(type !== 'sine' && type !== 'triangle' && type != 'square' && type != 'sawtooth') {
+            throw `The waveform can be of the following type: sine, square, triangle or sawtooth.`;
+        }
+
+        const oscillator = oscillators[name] || {};
+        oscillator.definition = {type: type, freq: freq};
+        const node = ctx.createOscillator();
+        node.frequency.value = freq;
+        node.type = type;
+
+        node.connect(gains.master);
+
+        oscillator.node = node;
+        createAnalyser(name, oscillator.node);
+
+        oscillators[name] = oscillator;
+    }
+
+    audio.play = function(name) {
+        
+        if(!name) {
+            throw `The name parameter is mandatory.`;
+        }
+
+        name = name.toLowerCase();
+
+        if(!oscillators[name]) {
+            throw `The oscillator ${name} does not exist.`;
+        }
+
+        if(!oscillators[name].node) {
+            const def = oscillators[name].definition;
+            audio.createOscillator(name, def.type, def.freq);
+        } 
+
+        if(!isPlaying(analysers[name])) {
+            oscillators[name].node.start();
+        }
+    }
+
+    audio.pause = function(name) {
+
+        if(!name) {
+            throw `The name parameter is mandatory.`;
+        }
+
+        name = name.toLowerCase();
+
+        if(!oscillators[name]) {
+            throw `The oscillator ${name} does not exist.`;
+        }
+
+        if(!oscillators[name].node) {
+            throw `The oscillator ${name} is already paused.`;
+        }
+
+        if(isPlaying(analysers[name])) {
+            deleteAnalyser(name, oscillators[name].node);
+            oscillators[name].node.stop();
+            oscillators[name].node = null;            
+        }
+    }
+
+    audio.setMaster = function(value) {
+        const master = gains.master;
+        if( value < master.minValue || master.maxValue < value) {
+            throw `the gain value ${value} is out of bound.`;
+        }
+
+        gains.master.gain.setValueAtTime(value, ctx.currentTime);
+    }
+
+    audio.getFloatFrequency = function(name) {
+        if(!name) {
+            throw `The name parameter is mandatory.`;
+        }
+
+        name = name.toLowerCase();
+        const analyser = analysers[name];
+
+        if(!analyser) {
+            throw `The is no analyser for oscillator ${name}`;
+        }
+
+        const array = new Float32Array(analyser.frequencyBinCount);
+        analysers[name].getFloatFrequencyData(array);
+        return array;
+    }
+
+    audio.getFloatTimeData = function(name) {
+        if(!name) {
+            throw `The name parameter is mandatory.`;
+        }
+
+        name = name.toLowerCase();
+        const analyser = analysers[name];
+
+        if(!analyser) {
+            throw `The is no analyser for oscillator ${name}`;
+        }
+
+        const array = new Float32Array(analyser.fftSize);
+        analysers[name].getFloatTimeDomainData(array);
+        return array;
+    }
+
+    return audio;
+})();
+
+const visualiser = (() => {
+
+})();
+
+(() => {
+    console.log(audio);
+})();
